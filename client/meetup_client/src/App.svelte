61e005665da69bcbb99bb3d6eb61ceefe0025dc2
mudjaycker <tombutoyi@gmail.com>
2022-02-28 15:51:05 +0200

    Post Request and Appending Meetups

--- client/meetup_client/src/App.svelte
+++ client/meetup_client/src/App.svelte
@@ -8,14 +8,42 @@
     const url = "http://127.0.0.1:5000/api/1/meetup";
     let meetups = [];
 
+    let title = "";
+    let subtitle = "";
+    let address = "";
+    let imageurl = "";
+    let email = "";
+
     let getMeetUps = async () => {
         meetups = await axios
             .get(url)
             .then((res) => res.data.meetups)
             .catch((err) => console.log(err));
-        console.log(meetups);
     };
-    getMeetUps(); 
+
+    function setBlank() {
+        title = "";
+        subtitle = "";
+        address = "";
+        imageurl = "";
+        email = "";
+    }
+    getMeetUps();
+    const addMeetup = () => {
+        axios
+            .post(url, {
+                title: title,
+                subtitle: subtitle,
+                address: address,
+                imageurl: imageurl,
+                email: email,
+            })
+            .then((res) => {
+                getMeetUps();
+                setBlank();
+            })
+            .catch((err) => console.log(err));
+    };
 </script>
 
 <main>
@@ -32,6 +60,97 @@
             />
         {/each}
     </div>
+
+    <form on:submit|preventDefault={addMeetup} action="">
+        <div class="container is-max-desktop mt-6">
+            <div class="field has-addons">
+                <div class="control">
+                    <spam class="button is-static"
+                        ><p class="subtitle is-4">Title</p>
+                    </spam>
+                </div>
+                <div class="control">
+                    <input
+                        bind:value={title}
+                        class="input"
+                        placeholder="placeholder"
+                        type="text"
+                    />
+                </div>
+            </div>
+
+            <div class="field has-addons">
+                <div class="control">
+                    <spam class="button is-static">
+                        <p class="subtitle is-4">Subtitle</p>
+                    </spam>
+                </div>
+                <div class="control">
+                    <input
+                        bind:value={subtitle}
+                        class="input"
+                        placeholder="placeholder"
+                        type="text"
+                    />
+                </div>
+            </div>
+
+            <div class="field has-addons">
+                <div class="control">
+                    <spam class="button is-static">
+                        <p class="subtitle is-4">Email</p>
+                    </spam>
+                </div>
+                <div class="control">
+                    <input
+                        bind:value={email}
+                        class="input"
+                        placeholder="placeholder"
+                        type="email"
+                    />
+                </div>
+            </div>
+
+            <div class="field has-addons">
+                <div class="control">
+                    <spam class="button is-static">
+                        <p class="subtitle is-4">Address</p>
+                    </spam>
+                </div>
+                <div class="control">
+                    <input
+                        bind:value={address}
+                        class="input"
+                        placeholder="placeholder"
+                        type="text"
+                    />
+                </div>
+            </div>
+
+            <div class="field has-addons">
+                <div class="control">
+                    <spam class="button is-static is-responsive">
+                        <p class="subtitle is-4">Image URL</p>
+                    </spam>
+                </div>
+                <div class="control">
+                    <input
+                        bind:value={imageurl}
+                        class="input"
+                        placeholder="placeholder"
+                        type="text"
+                    />
+                </div>
+            </div>
+
+            <button
+                type="submit"
+                class="button is-responsive is-success is-outlined"
+            >
+                add</button
+            >
+        </div>
+    </form>
 </main>
 
 <style>
